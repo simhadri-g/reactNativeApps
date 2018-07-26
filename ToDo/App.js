b95061356887b42bf2c97bdc2c03a0f3d7cb39f3
Guardian-of-code <simhadri064@gmail.com>
2018-07-26 14:00:59 +0530

    update todo

--- ToDo/App.js
+++ ToDo/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
 export default class App extends React.Component {
   state =
   {
-    text:"This is our initial text",
+    text:"",
     todo:["Learn react","learn react native"]
     }
   addTodo=()=>{
@@ -14,11 +14,23 @@ export default class App extends React.Component {
     this.setState({todo:arr,text:""});
   }
 
+removeTodo=(t)=>{
+  var arr = this.state.todo;
+  var pos = arr.indexOf(t);
+  arr.splice(pos,1);
+  this.setState({todo:arr});
+}
+
 addRenderTodos=()=>{
   return(
     this.state.todo.map(t =>{
       return(
-        <Text key={t}>{t}</Text>
+        <Text
+        key={t}
+        onPress = {()=>this.removeTodo(t)}
+        >
+        {t}
+        </Text>
       )
     })
   )
@@ -32,6 +44,7 @@ addRenderTodos=()=>{
         style = {styles.inputStyle}
         onChangeText = {(text)=>this.setState({text})}
         value = {this.state.text}
+        placeholder = "Enter  event"
         />
         <Button
         title = "Add todo"
